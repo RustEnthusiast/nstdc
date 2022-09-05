ec915166bf7f445529af6f09c808abe0c325b677
Rust Enthusiast <RustEnthusiast@gmail.com>
2022-09-05 03:36:42 -0500

    Use `long` for `[u]int_least32_t` on Windows.

--- include/stddef.h
+++ include/stddef.h
@@ -4,18 +4,18 @@
 /// Null pointer constant.
 #define NULL 0
 
+/// Signed integer type returned when subtracting two pointers.
+#ifdef _WIN64
+typedef signed long long ptrdiff_t;
+#else
+typedef signed long ptrdiff_t;
+#endif
+
 /// Unsigned integer type returned by the `sizeof` operator.
 #ifdef _WIN64
 typedef unsigned long long size_t;
 #else
 typedef unsigned long size_t;
-#endif
-
-/// Signed integer type returned when subtracting two pointers.
-#ifdef _WIN64
-typedef long long ptrdiff_t;
-#else
-typedef long ptrdiff_t;
 #endif
 
 #endif
--- include/stdint.h
+++ include/stdint.h
@@ -6,7 +6,15 @@ typedef signed char int_least8_t;
 /// Smallest signed integer type with width of at least 16 bits.
 typedef signed short int_least16_t;
 /// Smallest signed integer type with width of at least 32 bits.
+#if defined(__WINDOWS__)\
+    || defined(_WIN32)\
+    || defined(_WIN64)\
+    || defined(__WIN32__)\
+    || defined(__TOS_WIN__)
+typedef signed long int_least32_t;
+#else
 typedef signed int int_least32_t;
+#endif
 /// Smallest signed integer type with width of at least 64 bits.
 typedef signed long long int_least64_t;
 
@@ -15,7 +23,15 @@ typedef unsigned char uint_least8_t;
 /// Smallest unsigned integer type with width of at least 16 bits.
 typedef unsigned short uint_least16_t;
 /// Smallest unsigned integer type with width of at least 32 bits.
+#if defined(__WINDOWS__)\
+    || defined(_WIN32)\
+    || defined(_WIN64)\
+    || defined(__WIN32__)\
+    || defined(__TOS_WIN__)
+typedef unsigned long uint_least32_t;
+#else
 typedef unsigned int uint_least32_t;
+#endif
 /// Smallest unsigned integer type with width of at least 64 bits.
 typedef unsigned long long uint_least64_t;
 
